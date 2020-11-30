de267d1e1c1be6c96920e5c183c937417a61566b
AnantaFerdinand <s11820001@student.unklab.ac.id>
2020-11-30 16:48:31 +0800

    5th commit

--- index.js
+++ index.js
@@ -3,23 +3,24 @@ let title = document.getElementsByTagName('h1').innerText;
 title = document.getElementById('header 2').innerText = "Student";
 console.log(title);
 
-let age = document.getElementById("age").value;
-console.log(age);
 
 
-function hello(){
-    let value = document.getElementById("age").value;
-    document.getElementsByTagName("p").innerText = value;
+
+function Website(){
+
+    document.getElementsByTagName("h1").innerText = value;
     document.querySelector(".td").style.fontFamily = "Times New Roman";
-    document.querySelector(".td").style.color = "greenlime";
+    document.querySelector(".td").style.color = ".greenlime";
     document.querySelector(".td").style.fontSize = "3em";
     document.querySelector(".td").style.backgroundColor ="black";
 
     document.querySelector(".h1").style.backgroundColor = "green";
+    document.querySelector(".body").style.position = "absolute";
+    document.querySelector(".body").style.fontSize = "5em";
 
     document.querySelector(".a").style.Color = "darkblue";
-    document.querySelector(".a").style.fontSize = "30px";
+    document.querySelector(".a").style.fontSize = "40px";
 
     let span = document.getElementsByTagName("span") [0];
-    span.style.color="red";
+    span.style.color="black";
 }
